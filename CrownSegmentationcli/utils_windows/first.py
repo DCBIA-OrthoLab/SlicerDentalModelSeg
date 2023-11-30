4973011956b677ae5160bf9757558d859c9a312c
GaelleLeroux <gaelleleroux974@gmail.com>
2023-11-30 14:17:54 -0500

    STYLE : delete old command

--- CrownSegmentationcli/utils_windows/first.py
+++ CrownSegmentationcli/utils_windows/first.py
@@ -99,19 +99,7 @@ def createCondaEnv(name:str) :
 
     path_pip=f"~/miniconda3/envs/{name}/bin/pip"
     path_activate = f"~/miniconda3/bin/activate"
-    install_commands = [
-    f"wsl -- bash -c \"source {path_activate} {name} && ~/miniconda3/bin/conda install -y -c conda-forge cudnn\"",
-    f"wsl -- bash -c \"source {path_activate} {name} && {path_pip} install tqdm==4.64.0\"",
-    f"wsl -- bash -c \"source {path_activate} {name} && {path_pip} install pandas==1.4.2\"",
-    f"wsl -- bash -c \"source {path_activate} {name} && {path_pip} install torch==1.12.0 torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu113\"",
-    f"wsl -- bash -c \"source {path_activate} {name} && {path_pip} install monai==0.7.0\"",
-    f"wsl -- bash -c \"source {path_activate} {name} && {path_pip} install pytorch_lightning==2.1\"",
-    f"wsl -- bash -c \"source {path_activate} {name} && {path_pip} install fvcore==0.1.5.post20220504\"",
-    f"wsl -- bash -c \"source {path_activate} {name} && {path_pip} install rpyc\"",
-    f"wsl -- bash -c \"source {path_activate} {name} && {path_pip} install vtk\"",
-    f"wsl -- bash -c \"source {path_activate} {name} && {path_pip} install iopath==0.1.9\"",
-    f"wsl -- bash -c \"source {path_activate} {name} && {path_pip} install --force-reinstall --no-deps --no-index --no-cache-dir pytorch3d -f https://dl.fbaipublicfiles.com/pytorch3d/packaging/wheels/py39_cu113_pyt1120/download.html\""
-    ]
+
     
     install_commands = [
     f"wsl -- bash -c \"source {path_activate} {name} && {path_pip} install tqdm==4.64.0\"",
